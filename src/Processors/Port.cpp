3fe68e6705aea93a6d98b856a852e11ac13b3e0f
Vico.Wu <583424568@qq.com>
2025-04-23 21:24:54 +0800

    fix compile isue

--- src/Processors/Port.cpp
+++ src/Processors/Port.cpp
@@ -7,7 +7,12 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
 }
-
+/**
+ * 连接两个port
+ * @param output
+ * @param input
+ * @param reconnect
+ */
 void connect(OutputPort & output, InputPort & input, bool reconnect)
 {
     if (!reconnect && input.state)
@@ -15,15 +20,14 @@ void connect(OutputPort & output, InputPort & input, bool reconnect)
 
     if (!reconnect && output.state)
         throw Exception(ErrorCodes::LOGICAL_ERROR, "Port is already connected, (header: [{}])", output.header.dumpStructure());
-
     auto out_name = output.processor ? output.getProcessor().getName() : "null";
     auto in_name = input.processor ? input.getProcessor().getName() : "null";
 
     assertCompatibleHeader(output.getHeader(), input.getHeader(), fmt::format("function connect between {} and {}", out_name, in_name));
 
-    input.output_port = &output;
-    output.input_port = &input;
-    input.state = std::make_shared<Port::State>();
+    input.output_port = &output;// 将这个input的output_port指向output
+    output.input_port = &input; // 将这个output的input_port指向input
+    input.state = std::make_shared<Port::State>(); // 为两个Port之间的连接构建共享信息
     output.state = input.state;
 }
 
